4fc51235d93bebc8e0f350b3c969f6325f96cc28
shohjahon-sohibov <sohibov.s@bk.ru>
2022-07-02 10:46:47 +0500

    add tatic file "upload"

--- api/server.js
+++ api/server.js
@@ -2,10 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongo = require("../utils/db");
+const cors = require('cors')
 
 const productRoutes = require("../routes/productRoutes");
 
 app.use(express.json());
+app.use(cors());
+app.use('/upload', express.static('upload'))
+app.use(express.urlencoded({ extended: true }));
+
 mongo()
   .then(() => console.log("Connected"))
   .catch((err) => console.log(err));
